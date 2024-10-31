4ab1430874ade1166e9229a565686a61e2288128
DocDuvalier <zavref00@jcu.cz>
2024-10-31 09:30:02 +0000

    hodina2

---
+++ hodina/cviceni5-1.py
@@ -0,0 +1,30 @@
+import sys
+import csv
+
+with open("excel.csv", "excel2.csv") as file:
+    reader = csv.reader(file)
+    for radek in reader:
+        print(radek)
+
+def nacti_csv(soubor):
+    pass
+
+
+def spoj_data(*data):
+    pass
+
+
+def zapis_csv(soubor, data):
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        soubor1 = sys.argv[1]
+        soubor2 = sys.argv[2]
+        csv_data1 = nacti_csv(soubor1)
+        csv_data2 = nacti_csv(soubor2)
+        vysledna_data = spoj_data(csv_data1, csv_data2)
+        zapis_csv(vysledna_data)
+    except Exception:
+        pass
