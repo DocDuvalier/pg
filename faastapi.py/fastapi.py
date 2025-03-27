134661461807130611c61056d7cd9dafd491945f
DocDuvalier <zavref00@jcu.cz>
2025-03-27 09:03:02 +0000

    cer

---
+++ faastapi.py/fastapi.py
@@ -0,0 +1,2 @@
+#pip install fastapi pip install pidantic pip install unicorn
+from pidantic import BasdeModel, 
---
+++ faastapi.py/fastapimain.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI,
+from models import Flight
+
+app = FastAPI()
